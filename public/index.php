bc6815f94320575bac98f62aece562cd6fa4f700
ebcms <1540837821@qq.com>
2020-08-23 09:11:44 +0800

    1

--- public/index.php
+++ public/index.php
@@ -4,9 +4,9 @@ use Ebcms\App;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-if (version_compare(PHP_VERSION, '7.2.0', '<')) {
+if (version_compare(PHP_VERSION, '7.1.0', '<')) {
     header("Content-type: text/html; charset=utf-8");
-    die('PHP >= 7.2');
+    die('PHP >= 7.1');
 }
 
 App::getInstance()->run();
